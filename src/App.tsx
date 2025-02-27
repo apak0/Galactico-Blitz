8ec828a60196f31e10fe238842d50fe8fc24cf9a
apak0 <muhampak25@gmail.com>
2025-02-27 15:45:58 +0300

    version information located to bottom right side

--- src/App.tsx
+++ src/App.tsx
@@ -370,7 +370,7 @@ function App() {
       <div className="absolute top-4 right-4 text-white text-xl font-bold">
         Time: {timeLeft}s
       </div>
-      <div className="absolute top-0 right-0 text-white text-sm font-normal">
+      <div className="absolute bottom-0 right-0 text-white text-xs font-normal">
         v2.0
       </div>
       <div
